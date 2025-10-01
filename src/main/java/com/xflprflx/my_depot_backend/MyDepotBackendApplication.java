e10bc044cdec86b9a345b4d14495e68605efa467
xflprflx <x.filipe.machado.x@gmail.com>
2025-09-30 21:21:56 -0300

    projeto criado

---
+++ src/main/java/com/xflprflx/my_depot_backend/MyDepotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xflprflx.my_depot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDepotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDepotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xflprflx/my_depot_backend/MyDepotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xflprflx.my_depot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDepotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
