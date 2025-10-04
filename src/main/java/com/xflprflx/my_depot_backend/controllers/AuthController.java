82b5c314b6ca38fcb759fdc158d977ea539fc1b8
xflprflx <x.filipe.machado.x@gmail.com>
2025-10-04 10:53:39 -0300

    corrige rota de api

--- src/main/java/com/xflprflx/my_depot_backend/controllers/AuthController.java
+++ src/main/java/com/xflprflx/my_depot_backend/controllers/AuthController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final RestTemplate restTemplate = new RestTemplate();
